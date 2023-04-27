096c9916438268e851cfa1573e47e4d14cd1c978
diasEric04 <ericarrudadias@gmail.com>
2023-04-27 10:54:01 -0300

    initial

---
+++ src/Pages/App/index.tsx
@@ -0,0 +1,13 @@
+//react Imports
+import { ReactElement } from "react";
+
+//component imports
+
+//css imports
+import './css/styles.css'
+
+export default function App () : ReactElement {
+    return (
+        <div>teste</div>
+    )
+}
